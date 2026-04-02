3f7fb91605b38f1f377673b3e05dbca5dc4a3952
EricHafemann <eric070gabriel@gmail.com>
2026-04-02 08:39:03 -0300

    feat(enum): adiciona atributos a enum de TipoUsuario.

--- src/main/java/com/agrotag/network/domain/entities/enums/TipoUsuario.java
+++ src/main/java/com/agrotag/network/domain/entities/enums/TipoUsuario.java
@@ -1,6 +1,41 @@
 package com.agrotag.network.domain.entities.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public enum TipoUsuario {
 
+    ADMIN(0, "Usuário com permissão total"),
+    NORMAL_USER(1, "Usuário com acesso a suas informações");
 
+    private final int codigo;
+    private final String descricao;
+
+    @JsonValue
+    public int getCodigo() {
+        return codigo;
+    }
+
+    @JsonCreator
+    public static TipoUsuario fromCodigo(int codigo) {
+        for (TipoUsuario tipo : TipoUsuario.values()) {
+            if (tipo.codigo == codigo) {
+                return tipo;
+            }
+        }
+        throw new IllegalArgumentException("Código inválido: " + codigo);
+    }
+
+    public static TipoUsuario fromDescricao(String descricao) {
+        for (TipoUsuario tipo : TipoUsuario.values()) {
+            if (tipo.descricao.equalsIgnoreCase(descricao)) {
+                return tipo;
+            }
+        }
+        throw new IllegalArgumentException("Descrição inválida: " + descricao);
+    }
 }
